39c4fc1a0491790e917f94a7d034785290184a3a
kugushina <gkugushina@gmail.com>
2015-10-29 14:34:55 +0300

    1. Подготовить инфраструктуру для локального запуска и разработки тестов
    С переменной M2_REPO проблем не возникло, настраивать не пришлось.

---
+++ src/test/java/kugushina/FirstHomework.java
@@ -0,0 +1,57 @@
+package kugushina;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FirstHomework extends TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testUntitled() throws Exception {    driver.get(baseUrl + "/php4dvd/");
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Add movie\"]")).click();
+    driver.findElement(By.id("imdbsearch")).clear();
+    driver.findElement(By.id("imdbsearch")).sendKeys("lord");
+    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+    driver.findElement(By.xpath("//td[@onclick=\"location.href='./?go=add&imdbid=0120737';\"]")).click();
+    driver.findElement(By.id("cover")).clear();
+    driver.findElement(By.id("cover")).sendKeys("C:\\Users\\galina.kugushina\\Desktop\\Ringstrilogyposter.jpg");
+    driver.findElement(By.id("submit")).click();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
