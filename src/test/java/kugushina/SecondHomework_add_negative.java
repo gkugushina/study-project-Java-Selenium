120b4acf151814e92e0e3246083c2628726b4e63
kugushina <gkugushina@gmail.com>
2015-11-05 20:02:12 +0300

    3. Разработать тесты для добавления и удаления фильмов

---
+++ src/test/java/kugushina/SecondHomework_add_negative.java
@@ -0,0 +1,61 @@
+package kugushina;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SecondHomework_add_negative extends TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testUntitled() throws Exception {    
+	  driver.get(baseUrl + "/php4dvd/");
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Add movie\"]")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Марсианин");
+    if(driver.findElement(By.name("year")).getAttribute("value").isEmpty() || 
+    		driver.findElement(By.name("name")).getAttribute("value").isEmpty()) {
+        throw new Error("Данные некорректны");
+      }
+    else
+    {driver.findElement(By.cssSelector("img[alt=\"Save\"]")).click();}
+    
+  }
+
+  
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
